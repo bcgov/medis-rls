eee4bcf06cbf5048d86e1e953664bb49e0f8e93e
Sergey Fedyanin <s.fedyanin.bc@gmail.com>
2024-09-12 23:12:52 +0000

    fetch CHEFS submission data via External API

--- app/src/forms/external/controller.js
+++ app/src/forms/external/controller.js
@@ -1,5 +1,8 @@
+const axios = require('axios');
 const formService = require('../form/service');
 const userService = require('../user/service');
+
+const CHEFS_API_ENDPOINT = 'https://submit.digital.gov.bc.ca/app/api/v1';
 
 module.exports = {
   listFormSubmissions: async (req, res, next) => {
@@ -8,8 +11,26 @@ module.exports = {
       const idpUserId = req.headers['x-chefs-user-userid'];
       const userRls = await userService.readByIdpUserId(idpUserId);
       const formId = req.query?.extFormId;
-      const response = await formService.listFormSubmissions(formId, req.query, userRls, remoteFormId, true);
-      res.status(200).json(response);
+      // if req.extFormApiKey exist we need to fetch CHEFS submission data instead
+      if (req.extFormApiKey) {
+        const axiosOptions = { timeout: 10000 };
+        const axiosInstance = axios.create(axiosOptions);
+        const basicAuthEncoded = Buffer.from(formId + ':' + req.extFormApiKey).toString('base64');
+        axiosInstance.interceptors.request.use(
+          (cfg) => {
+            cfg.headers = { authorization: `Basic ${basicAuthEncoded}` };
+            return Promise.resolve(cfg);
+          },
+          (error) => {
+            return Promise.reject(error);
+          }
+        );
+        const remoteSubmissionData = await axiosInstance.get(`${CHEFS_API_ENDPOINT}/forms/${formId}/export?format=json&type=submissions`);
+        res.status(200).json(remoteSubmissionData?.data);
+      } else {
+        const response = await formService.listFormSubmissions(formId, req.query, userRls, remoteFormId, true);
+        res.status(200).json(response);
+      }
     } catch (error) {
       next(error);
     }
